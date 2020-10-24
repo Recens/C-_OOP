1da4a0688fafb707541d2d6f02048e65ea962e2b
Acer <acer.citrus1501@outlook.com>
2020-10-24 21:15:18 +0300

    Symmetry

--- LB_2/№3/№3.cpp
+++ LB_2/№3/№3.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <string>
-#define M_PI 3.14159265358979323846
+#define PI 3.14159265358979323846
 
 
 class Point
@@ -67,7 +67,7 @@ public:
 	}
     Circle(float radius, int xpos, int ypos)
 	{
-        area = M_PI * radius * radius;
+        area = PI * radius * radius;
         std::cout << "circle area = "<< area<<std::endl;
 	}
 };
@@ -95,9 +95,28 @@ class Rectangle : public Polygon
 {
 protected:
 public:
-    
+	public:
+        double sym = 0;
+		Rectangle(const Point& first, const Point& second, const Point& third, const Point& forth)
+        {
+		    std::cout << "Rectangle created\n";
+		    area = Segment::len(first, second) * Segment::len(second, third);
+            std::cout << "Rectangle area = " << area << std::endl;
+		}
+        void symmetry(Point& first, Point& second, Point& third, Point forth, const Point& symm)
+		{
+            sym = Segment::len(first, symm) * 2;
+            first.xpos = first.xpos + sym;
+            second.xpos = second.xpos+sym;
+            third.xpos = third.xpos + sym;
+            forth.xpos = forth.xpos + sym;
 
-
+            first.ypos = first.ypos + sym;
+            second.ypos = second.ypos * sym;
+            third.ypos = third.ypos * sym;
+            forth.ypos = forth.ypos * sym;
+            std::cout << "symm = "<<first.xpos << first.ypos << std::endl;
+		}
 };
 
 class Triangle : public Polygon
@@ -130,6 +149,15 @@ public:
             std::cout << "triangle not correct\n";
         }
     }
+    void parallel(Point& firstApex, Point& secondApex, Point& thirdApex, const Point& Vector) {
+        firstApex.xpos += Vector.xpos;
+        secondApex.xpos += Vector.xpos;
+        thirdApex.xpos += Vector.xpos;
+
+        firstApex.ypos += Vector.ypos;
+        secondApex.ypos += Vector.ypos;
+        thirdApex.ypos += Vector.ypos;
+    }
 
 };
 
@@ -137,21 +165,36 @@ public:
 
 int main()
 {
-    Point pobj(4,5);
+    Point point (4,5);
+	
     Point p_1(3,4);
     Point p_2(4, 5);
-    Point t_1(5, 2);
-    Point t_2(8, 3);
-	Point t_3(10, 11);
-    std::cout << "segment length = " <<Segment::len(p_1, p_2)<<std::endl;
-    Circle c_1(4, 3, 7);
+    std::cout << "segment length = " << Segment::len(p_1, p_2) << std::endl;
+	
+    Point t_1(0, 0);
+    Point t_2(4, 1);
+	Point t_3(2, 5);
+    Point vector(5,5);
     Triangle t(t_1, t_2, t_3);
+    t.parallel(t_1, t_2, t_3, vector);
+	
+    Circle c_1(4, 3, 7);
+	
+   
     Point pl_1(3,1);
     Point pl_2(2,3);
     Point pl_3(5,6);
     Point pl_4(8,3);
     Point pl_5(7,1);
     Polygon pl(pl_1, pl_2, pl_3, pl_4, pl_5);
+
+    Point rc_1(0,0);
+    Point rc_2(0,4);
+    Point rc_3(6,4);
+    Point rc_4(6,0);
+    Point symm(5,5);
+    Rectangle rec(rc_1,rc_2,rc_3,rc_4);
+    rec.symmetry(rc_1, rc_2, rc_3, rc_4, symm);
 	
 
 }
