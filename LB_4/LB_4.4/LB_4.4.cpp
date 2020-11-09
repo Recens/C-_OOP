442cb45b1ba01d49f9787773b10b550af3da26f8
Acer <acer.citrus1501@outlook.com>
2020-11-09 18:11:51 +0200

    fix

--- LB_4/LB_4.4/LB_4.4.cpp
+++ LB_4/LB_4.4/LB_4.4.cpp
@@ -5,31 +5,34 @@ private:
     int* size;
     
 public:
-    int coordinat[400];
+    int cordinat[400];
     Point() {};
     Point(int size) {
         int value;
         this->size = new int(size);
         for (int i = 0; i < getsize() * 2; i++) {
-            std::cout << "Please, enter coordinate: " << (i + 1)<< " ";
+            std::cout << " Enter coordinate: " << (i + 1)<< " ";
             std::cin >> value;
-            setcoordinat(value, i);
+            std::cout<<"\n";
+
+            setcordinat(value, i);
         }
+
     }
 
     int getsize() {
         return *(size);
     }
 
-    void setcoordinat(int value, int i) {
-        coordinat[i] = value;
+    void setcordinat(int value, int i) {
+        cordinat[i] = value;
     }
 
 
-    std::string getcoordinat()
+    std::string getcordinat()
 	{
         for (int i = 0; i < getsize() * 2; i++) {
-            std::cout << coordinat[i] << " ";
+            std::cout << cordinat[i] << " ";
             
         }
         return "\n";
@@ -45,18 +48,18 @@ public:
         int value;
         std::cout << "Element Composition: " << "\n";
         for (int i = 0; i < first.getsize() * 2; i++) {
-            value = (first.coordinat[i] + second.coordinat[i]);
-            std::cout << "value : " << value << "\n";
-            Third.setcoordinat(value, i);
+            value = (first.cordinat[i] + second.cordinat[i]);
+            std::cout << "value: " << value << "\n";
+            Third.setcordinat(value, i);
         }
     }
     void Substraction(Point exampleOne, Point exampleTwo, Point& Third) {
         int value;
         std::cout << "Element Subtractions: " << "\n";
         for (int i = 0; i < exampleOne.getsize() * 2; i++) {
-            value = (exampleOne.coordinat[i] - exampleTwo.coordinat[i]);         
-            std::cout << "value : " << value << "\n";
-            Third.setcoordinat(value,i);
+            value = (exampleOne.cordinat[i] - exampleTwo.cordinat[i]);
+            std::cout << "value: " << value << "\n";
+            Third.setcordinat(value, i);
         }
     }
 };
@@ -65,16 +68,15 @@ int main()
 {
     Point v_1(2);
     Point v_2(2);
-    std::cout << v_1.getcoordinat();
-    std::cout << v_2.getcoordinat();
+    std::cout << v_1.getcordinat();
+    std::cout << v_2.getcordinat();
     Point v_3;
     Point sub;
     v_3 = Point::compositionObject(v_2, v_1);
     sub = Point::compositionObject(v_1, v_2);
     v_3.Composition(v_1, v_2, v_3);
 	sub.Substraction(v_1, v_2, sub);
-    std::cout << " - Composition vector" << "\4" << v_3.getcoordinat() << " " << "\n";
-    std::cout << " - Substraction vector" <<"\4"<< sub.getcoordinat() << " ";
-    std::cout << "\n";
+    std::cout << "Composition vector: " << v_3.getcordinat() << " " << "\n";
+    std::cout << "Substraction vector: " << sub.getcordinat() << " " << "\n";
     std::cout << "Hello World!\n";
 }
